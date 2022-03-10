dbf9c6090f387b90d6a4d1e3d79a88e6a9660202
karimovsamandar <karimovsamandar013@gmail.com>
2022-03-10 21:32:18 +0300

    commit on main

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
-var sampleText = "JavaScript is a must-know  programming  language for a modern developer";
-  
-
 function modifyText(sampleText, sep) {
     return sampleText.split(sep).reverse().join(sep);
-  }
-    
-  result1 = modifyText(sampleText, "");
-  result2 = modifyText(sampleText , " ");
-  
-  console.log(result1);
-  console.log(result2);
+}
+var sampleText = "JavaScript is a must-know  programming  language for a modern developer";
+
+result1 = modifyText(sampleText, "");
+result2 = modifyText(sampleText , " ");
+
+console.log(result1);
+console.log(result2);
